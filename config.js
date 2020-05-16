7cc302351c29c4c7b3dd2301757560287cb9bebe
Mark Prykhoda <hroscko@gmail.com>
2020-05-16 11:52:24 +0300

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
 	secretKey: "12345-67890-09876-54321",
 	mongoUrl: "mongodb://localhost:27017/conFusion",
+	facebook: {
+		clientId: "581747709130159",
+		clientSecret: "6ac4d9e643ebe7a088d9293db3d7f654",
+	},
 };
